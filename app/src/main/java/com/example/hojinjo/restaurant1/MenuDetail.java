bd784d3b339bdc19bad9502da3fd41ed2c04fa69
ryujunghyun <rjh1996@hanmail.net>
2017-10-14 22:26:50 +0900

    위로가기버튼
    manifest.xml에 부모패런트 등록, 액션바 정의하여 위로돌아가기 버튼 생성

--- app/src/main/java/com/example/hojinjo/restaurant1/MenuDetail.java
+++ app/src/main/java/com/example/hojinjo/restaurant1/MenuDetail.java
@@ -1,6 +1,7 @@
 package com.example.hojinjo.restaurant1;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -19,6 +20,11 @@ public class MenuDetail extends AppCompatActivity {
 
         TextView tvprice = (TextView)findViewById(R.id.textView2);
 
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
         Intent intent = getIntent();
 
         tvmenu.setText(intent.getStringExtra("menu"));
