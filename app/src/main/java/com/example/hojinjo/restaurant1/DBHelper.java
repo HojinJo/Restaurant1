1db874f59d434eb198cd0ccef8d0a4c610932130
HojinJo <halo1206@naver.com>
2017-11-16 18:18:48 +0900

    DB생성

---
+++ app/src/main/java/com/example/hojinjo/restaurant1/DBHelper.java
@@ -0,0 +1,44 @@
+package com.example.hojinjo.restaurant1;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+public class DBHelper extends SQLiteOpenHelper {
+    final static String TAG="SQLiteDBTest";
+
+    public DBHelper(Context context) {
+        super(context, RContract.DB_NAME, null, RContract.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i(TAG,getClass().getName()+".onCreate()");
+        db.execSQL(RContract.Restaurant.CREATE_TABLE);       //데이터베이스가 없을때
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        Log.i(TAG,getClass().getName() +".onUpgrade()");
+        db.execSQL(RContract.Restaurant.DELETE_TABLE);
+        onCreate(db);
+    }
+
+    public long insertUserByMethod(String name, String phone) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(RContract.Restaurant.KEY_NAME, name);
+        values.put(RContract.Restaurant.KEY_ADDRESS, name);
+        values.put(RContract.Restaurant.KEY_PHONE,phone);
+
+        return db.insert(RContract.Restaurant.TABLE_NAME,null,values);
+    }
+    public Cursor getAllUsersByMethod() {
+        SQLiteDatabase db = getReadableDatabase();
+        return db.query(RContract.Restaurant.TABLE_NAME,null,null,null,null,null,null);
+    }
+}
---
+++ app/src/main/java/com/example/hojinjo/restaurant1/RContract.java
@@ -0,0 +1,28 @@
+package com.example.hojinjo.restaurant1;
+
+
+import android.provider.BaseColumns;
+
+public final class RContract {
+    public static final String DB_NAME="restaurant.db";
+    public static final int DATABASE_VERSION = 1;
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private RContract() {}
+
+    /* Inner class that defines the table contents */
+    public static class Restaurant implements BaseColumns {
+        public static final String TABLE_NAME="Restaurant";
+        public static final String KEY_NAME = "Name";
+        public static final String KEY_ADDRESS = "address";
+        public static final String KEY_PHONE = "Phone";
+
+        public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY" + COMMA_SEP +
+                KEY_NAME + TEXT_TYPE + COMMA_SEP + KEY_ADDRESS + TEXT_TYPE + COMMA_SEP +
+                KEY_PHONE + TEXT_TYPE +  " )";
+        public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+}
